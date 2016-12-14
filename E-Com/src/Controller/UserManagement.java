df6a23b38fc9e346e99795776105ade272eb7fbd
AbdallahAhmed <abdallah.se95@gmail.com>
2016-12-15 01:37:04 +0200

    z2

--- E-Com/src/Controller/UserManagement.java
+++ E-Com/src/Controller/UserManagement.java
@@ -5,62 +5,65 @@ import DataAccess.UserDBAccess;
 
 public class UserManagement {
 
-    public UserManagement() {
-    }
+	public UserManagement() {
+	}
 
-    public boolean signOut() {
-        // TODO implement here
-        return false;
-    }
+	public boolean signOut() {
+		// TODO implement here
+		return false;
+	}
 
-    public User signIn(User user) {
-        // TODO implement here
-    	UserDBAccess u = new UserDBAccess();
-    	if(u.checkUser(user));{
-    		u.signIn(user);
-    		System.out.print("Welcome, " + user.name);
-    		//Home.jsp
-    	}
-    	//InvalidLogin.jsp
-        return null;
-    }
+	public User signIn(User user) {
+		// TODO implement here
+		UserDBAccess u = new UserDBAccess();
+		if (u.checkUser(user))
+		{
+			u.signIn(user);
+			System.out.print("Welcome, " + user.name);
+			// Home.jsp
+		}
+		// InvalidLogin.jsp
+		return null;
+	}
 
-    public boolean addUser(User user) {
-        // TODO implement here 
-    	UserDBAccess u = new UserDBAccess();
-    	if(!u.isUserFound(user)){
-    		u.addUser(user);
-    		return true;
-    }
-    	else
-    		return false;
-    }
+	public boolean addUser(User user) {
+		// TODO implement here
+		UserDBAccess u = new UserDBAccess();
+		if (!u.isUserFound(user)) {
+			u.addUser(user);
+			return true;
+		} else
+			return false;
+	}
 
-    public boolean removeUser(User user) {
-        // TODO implement here
-    	UserDBAccess u = new UserDBAccess();
-    	if(u.isUserFound(user))
-    		u.removeUser(user.username);
-    	else
-    		return false;
-    }
+	public boolean removeUser(User user) {
+		// TODO implement here
+		UserDBAccess u = new UserDBAccess();
+		if (u.isUserFound(user)) {
+			u.removeUser(user.username);
+			return true;
+		} else
+			return false;
+	}
 
-    public boolean editUser(User user) {
-        // TODO implement here
-    	UserDBAccess u = new UserDBAccess();
-    	if(u.isUserFound(user))
-    		u.editUser(user);
-    	else
-    		return false;
-    }
+	public boolean editUser(User user) {
+		// TODO implement here
+		UserDBAccess u = new UserDBAccess();
+		if (u.isUserFound(user)) {
+			u.editUser(user);
+			return true;
+		} else
+			return false;
+	}
 
-    public boolean rateTeacher(int rate, Teacher teacher) {
-        // TODO implement here
-    	UserDBAccess u = new UserDBAccess();
-    	if(u.isUserFound(teacher))
-    		u.rateTeacher(rate, teacher);
-    	else
-    		return false;
-    }
+	public boolean rateTeacher(int rate, Teacher teacher) {
+		// TODO implement here
+		UserDBAccess u = new UserDBAccess();
+		if (u.isUserFound(teacher)) {
+			u.rateTeacher(rate, teacher);
+			return true;
+		} else
+			return false;
+	}
 
 }
