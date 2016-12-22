52ac3ee23581b932b2b67d2e98b65cd0781db129
RanaMohamed <r.mohamed@stud.fci-cu.edu.eg>
2016-12-22 20:52:11 +0200

    Add semester

--- E-Com/src/DataAccess/SemesterDBAccess.java
+++ E-Com/src/DataAccess/SemesterDBAccess.java
@@ -1,52 +1,86 @@
 package DataAccess;
 
-import java.util.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import Controller.Semester;
 
 /**
  * 
  */
 public class SemesterDBAccess {
 
-    /**
-     * Default constructor
-     */
-    public SemesterDBAccess() {
-    }
+	/**
+	 * Default constructor
+	 */
+	public SemesterDBAccess() {
+	}
 
-    /**
-     * 
-     */
-    public Connection connect;
+	/**
+	 * 
+	 */
+	static Connection currentCon = null;
+	static ResultSet rs = null;
 
+	/**
+	 * @param studID
+	 * @param number
+	 * @return
+	 */
+	public boolean addSemester(int studID, int number) {
+		// TODO implement here
+		Statement stmt = null;
+		String Query = "insert into Semesters (StudentID , Number) values (" + studID + ", " + number + ")";
 
-    /**
-     * @param studID 
-     * @param number 
-     * @return
-     */
-    public boolean addSemester(int studID, int number) {
-        // TODO implement here
-        return false;
-    }
+		try {
+			currentCon = ConnectionManager.getConnection();
+			stmt = currentCon.createStatement();
+			stmt.executeUpdate(Query);
+		}
 
-    /**
-     * @param studID 
-     * @param number 
-     * @return
-     */
-    public Semester getSemester(int studID, int number) {
-        // TODO implement here
-        return null;
-    }
+		catch (Exception ex) {
+			System.out.println("AddSemester failed: An Exception has occurred! " + ex);
+		}
 
-    /**
-     * @param studID 
-     * @param number 
-     * @return
-     */
-    public boolean removeSemester(int studID, int number) {
-        // TODO implement here
-        return false;
-    }
+		// some exception handling
+		try {
+			if (rs != null) {
+				rs.close();
+				rs = null;
+			}
+			if (stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if (currentCon != null) {
+				currentCon.close();
+				currentCon = null;
+			}
+		} catch (Exception e) {
+
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param studID
+	 * @param number
+	 * @return
+	 */
+	public Semester getSemester(int studID, int number) {
+		// TODO implement here
+		return null;
+	}
+
+	/**
+	 * @param studID
+	 * @param number
+	 * @return
+	 */
+	public boolean removeSemester(int studID, int number) {
+		// TODO implement here
+		return false;
+	}
 
 }
