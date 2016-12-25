97e003d3c3a15b36e5da6351b64284762d3d7104
ismail <moly_meme@hotmail.com>
2016-12-25 19:55:48 +0200

    Sign out Servlet

--- E-Com/src/Controller/Sign_outServlet.java
+++ E-Com/src/Controller/Sign_outServlet.java
@@ -1,6 +1,13 @@
 package Controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * 
@@ -16,8 +23,23 @@ public class Sign_outServlet {
     /**
      * 
      */
-    public void Service() {
-        // TODO implement here
-    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		/* doGet(request, response); */
+		PrintWriter out = response.getWriter();
+		request.getRequestDispatcher("signin.jsp").include(request, response);
+		HttpSession session=request.getSession();  
+        session.invalidate();  
+        out.print("You are successfully logged out!");  
+        out.close(); 
+		
+	}
+
 
 }
