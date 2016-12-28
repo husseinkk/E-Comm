02d64d3fc1c613ba1087015bef49856906d2d7f0
RanaMohamed <r.mohamed@stud.fci-cu.edu.eg>
2016-12-28 11:42:29 +0200

    AddGrade

--- E-Com/src/DataAccess/Try.java
+++ E-Com/src/DataAccess/Try.java
@@ -1,16 +1,21 @@
 package DataAccess;
 
+import java.sql.Date;
+
 import Controller.Course;
+import Controller.Grade;
+import Controller.WorkYear;
 
 public class Try {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CourseDBAccess db = new CourseDBAccess();
-		Course[] courses = db.selectCourses(2);
-		for (Course c : courses) {
-			System.out.println(c.getCourseName() +" " + c.getDept() + " " + c.getHours() + " " + c.getCourseDescription());
-		}
+		GradeDBAccess db = new GradeDBAccess();
+		WorkYear g = new WorkYear();
+		g.description = "g";
+		g.score = 8;
+		g.type = "Task";
+		db.addGrade(g, "Network", 1);
 	}
 
 }
